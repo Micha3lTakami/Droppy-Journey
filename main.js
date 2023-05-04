d64a22b1236937186f8c1ec5f0faddbbc989b0a5
Michael Takami <mtakami@ucsc.edu>
2023-05-04 13:51:07 -0700

    setup

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 // main for Droppy's Journey
+
+// set screen size and starting scene
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [Loader, Menu, Play]
+  }
+// main game
+let game = new Phaser.Game(config);
+
+// reserve keyboard variables
+let keyLEFT, keyRIGHT;
+
+
--- src/scenes/loader.js
+++ src/scenes/loader.js
@@ -1,4 +1,34 @@
 // anything needed to pre load goes here
-preload(){
+class Loader extends Phaser.Scene {
+    constructor() {
+        super("loaderScene");
+    }
     
+    // preload()
+    // pre-load game assets
+    preload() {
+
+        // add text object for percentage loaded
+        let progressText = this.add.text(game.config.width/2, game.config.height/2, '0%', { fontFamily: 'Courier', fontSize: '48px', fill: '#fff' }).setOrigin(0.5);
+
+        // update the loading percentage as assets are loaded
+        this.load.on('progress', function (value) {
+            let percentage = Math.floor(value * 100) + '%';
+            progressText.setText(percentage); 
+        });
+
+        // load assets
+        //this.load.audio('sfx_select', './assets/blip_select12.wav');
+        this.load.image('CACA', './assets/CACA.png');
+        this.load.image('Can', './assets/can.png');
+        this.load.image('Droppy', './assets/droppy.png');
+        this.load.image('RottenFruit', './assets/rotten-apple-tomato.png')
+        this.load.image('shoe', './assets/shoe.png');
+
+        // change scene upon completion
+        this.load.on('complete', function () {
+            this.scene.start('menuScene');
+        }, this);
+
+    }
 }
