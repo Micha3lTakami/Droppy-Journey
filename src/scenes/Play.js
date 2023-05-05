4459a46bb78771ce5b2b5dee42784fd2afd68164
Michael Takami <mtakami@ucsc.edu>
2023-05-04 18:55:18 -0700

    tile sprite added

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -9,16 +9,7 @@ class Play extends Phaser.Scene {
     create() {
         // place tile sprite
         this.clouds = this.add.tileSprite(0, 0,480, 640, 'cloudBackg').setOrigin(0, 0);
-        /*
-        // green UI background
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
-        
-        // white borders
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        */
+
         
 
 
@@ -41,7 +32,7 @@ class Play extends Phaser.Scene {
     // update()
     // update object sprites throughout gameplay(can be thought of as constant update loop body)
     update() {
-
+        this.clouds.tilePositionY += 7;
         if(!this.gameOver) {
             this.droppy.update()
         }
